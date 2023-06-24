be378220c9e7f99daae8e913f39f352a6ca05dc4
RebelDot\cristian.socaci <cristian.socaci@rebeldot.com>
2023-06-24 20:59:02 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [response, setResponse] = useState("");
+
+  const endpoint = "http://localhost:7200/repositories/exam";
+
+  const query = `
+  prefix :      <http://buchmann.ro#>
+  prefix rdfs:  <http://www.w3.org/2000/01/rdf-schema#>
+  PREFIX mm: <http://bee-up.omilab.org/rdf/1_6#>
+  PREFIX cv: <http://www.comvantage.eu/mm#>
+  select ?name ?y where
+  {?x a mm:o_Pool_BPMN; rdfs:label ?name. 
+    ?y mm:r_Is_inside ?x; a mm:o_Sub-Process_BPMN;}
+  `;
+
+  useEffect(() => {
+    const params = new URLSearchParams();
+
+    params.append("query", query);
+
+    axios
+      .get(`${endpoint}?${params.toString()}`)
+      .then((res) => {
+        console.log(res);
+        console.log(res.data.results.bindings);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
